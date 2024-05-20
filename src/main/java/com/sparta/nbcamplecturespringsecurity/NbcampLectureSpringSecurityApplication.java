9ec10bb63b5a1ce90dd915f70741f93c99a4aeda
ssk910 <hanmir91@gmail.com>
2024-05-20 14:38:57 +0900

    Initial commit

---
+++ src/main/java/com/sparta/nbcamplecturespringsecurity/NbcampLectureSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamplecturespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampLectureSpringSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NbcampLectureSpringSecurityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/nbcamplecturespringsecurity/NbcampLectureSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamplecturespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampLectureSpringSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
